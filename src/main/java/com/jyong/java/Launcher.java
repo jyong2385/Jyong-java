01a3c8364fac5da3c40c0cc211006ed981d2f689
jyong <53065626+jyong2385@users.noreply.github.com>
2024-06-08 20:25:45 +0800

    Delete src directory

--- src/main/java/com/jyong/java/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.jyong.java;
-
-
-public class Launcher {
-    public static void main(String[] args) {
-        System.out.println("running ...");
-    }
-}
--- src/main/java/com/jyong/java/utils/ServerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.jyong.java.utils;
-
-public class ServerConfig {
-
-
-    public static String CLUSTER_NODE="192.168.92.103";
-    public static Integer REDIS_PORT=2181;
-}
